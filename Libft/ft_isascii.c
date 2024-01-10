f765efbf7415095e82faf426e7c71b5c7364d28d
daniel_mateu <danielmateu86@gmail.com>
2024-01-10 13:23:40 +0100

    Malefile updated

--- Libft/ft_isascii.c
+++ Libft/ft_isascii.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isascci.c                                       :+:      :+:    :+:   */
+/*   ft_isascii.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: damateu- <damateu-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 13:50:47 by damateu-          #+#    #+#             */
-/*   Updated: 2024/01/09 15:52:24 by damateu-         ###   ########.fr       */
+/*   Updated: 2024/01/10 13:19:56 by damateu-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ int ft_isascii(int c)
 }
 
 // Test main
+/*
 int main(void)
 {
     int c;
@@ -33,3 +34,4 @@ int main(void)
         printf("The character is not an ASCII character\n");
     return (0);
 }
+*/
--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: damateu- <damateu-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 10:53:47 by damateu-          #+#    #+#             */
-/*   Updated: 2024/01/10 10:58:56 by damateu-         ###   ########.fr       */
+/*   Updated: 2024/01/10 13:18:24 by damateu-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int ft_isalpha(int c);
 int ft_isalnum(int c);
 int ft_isdigit(int c);
 int ft_strlen(char *str);
-int ft_isascci(int c);
+int ft_isascii(int c);
 int ft_isprint(int c);
 int ft_toupper(int c);
 int ft_tolower(int c);
