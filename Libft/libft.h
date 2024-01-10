d5b4ee99be9c565e27ac924ee79b38718ed1236f
daniel_mateu <danielmateu86@gmail.com>
2024-01-10 13:34:21 +0100

    Makefile updated

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: damateu- <damateu-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 10:53:47 by damateu-          #+#    #+#             */
-/*   Updated: 2024/01/10 13:18:24 by damateu-         ###   ########.fr       */
+/*   Updated: 2024/01/10 13:24:25 by damateu-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,6 @@ int ft_isascii(int c);
 int ft_isprint(int c);
 int ft_toupper(int c);
 int ft_tolower(int c);
-char *fr_strchr(const char *s, int c);
+char *ft_strchr(const char *s, int c);
 
 #endif
