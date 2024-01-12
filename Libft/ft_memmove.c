ae6c6ce1dbd33f4abf7281f2b4c110de484fd59b
daniel_mateu <danielmateu86@gmail.com>
2024-01-12 14:42:06 +0100

    ft_memmove.c added

---
+++ Libft/ft_memmove.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: damateu- <damateu-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/12 14:20:28 by damateu-          #+#    #+#             */
+/*   Updated: 2024/01/12 14:23:44 by damateu-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dst, void const *src, size_t n)
+{
+    char *d;
+    char *s;
+
+    d = (char *)dst;
+    s = (char *)src;
+    if (s < d)
+    {
+        while (n--)
+            d[n] = s[n];
+    }
+    else
+        //ft_memcpy(d, s, n);
+        // Sin usar ft_memcpy
+    {
+        while (n--)
+            *d++ = *s++;
+    }
+    return (dst);
+}
