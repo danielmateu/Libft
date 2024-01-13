dadb5c126fe1dba9f531cc3913eea8fd0fb56d04
Daniel Mateu <danielmateu86@gmail.com>
2024-01-13 18:31:12 +0100

    ft_strlcpy updated

--- Libft/ft_strlcpy.c
+++ Libft/ft_strlcpy.c
@@ -6,18 +6,34 @@ size_t ft_strlcpy(char *dst, const char *src, size_t size)
 
     i = 0;
     if (size == 0)
-        //using ft_strlen
-        //return (ft_strlen(src));
-        // Without using ft_strlen
-
-        
-
+        return (ft_strlen(src));
     while (src[i] && i < size - 1)
     {
         dst[i] = src[i];
         i++;
     }
-    dst[i] = '\0';
-    //return (ft_strlen(src));
-    
+    if (size != 0)
+    {
+        dst[i] = '\0';
+    }
+    return (ft_strlen(src));
+}
+
+int ft_strlen(const char *str)
+{
+    int i;
+
+    i = 0;
+    while (str[i])
+        i++;
+    return (i);
+}
+
+int main()
+{
+    char *src = "Hello World";
+    char dst[5];
+    printf("%zu\n", ft_strlcpy(dst, src, 5));
+    printf("%s\n", dst);
+    return (0);
 }
