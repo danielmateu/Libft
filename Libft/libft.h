c8fc38c9044fa0416c1d31a284592f44c6206ac1
Daniel Mateu <danielmateu86@gmail.com>
2024-01-14 22:53:25 +0100

    ft_memchr.c

--- Libft/libft.h
+++ Libft/libft.h
@@ -11,29 +11,30 @@
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
-# define LIBFT_H
+#define LIBFT_H
 
-# include <stdio.h>
-# include <stdlib.h>
-# include <string.h>
-# include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #include <stddef.h>
 
-int		ft_isalpha(int c);
-int		ft_isalnum(int c);
-int		ft_isdigit(int c);
-int		ft_strlen(const char *str);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-int		ft_toupper(int c);
-int		ft_tolower(int c);
-char	*ft_strchr(const char *s, int c);
-char	*ft_strrchr(const char *s, int c);
-void	*ft_memset(void *b, int c, size_t len);
-void	*ft_memmove(void *dst, void const *src, size_t n);
-int		ft_strncmp(const char *str, const char *str2, size_t n);
-void    ft_bzero(void *s, size_t n);
+int ft_isalpha(int c);
+int ft_isalnum(int c);
+int ft_isdigit(int c);
+int ft_strlen(const char *str);
+int ft_isascii(int c);
+int ft_isprint(int c);
+int ft_toupper(int c);
+int ft_tolower(int c);
+char *ft_strchr(const char *s, int c);
+char *ft_strrchr(const char *s, int c);
+void *ft_memset(void *b, int c, size_t len);
+void *ft_memmove(void *dst, void const *src, size_t n);
+int ft_strncmp(const char *str, const char *str2, size_t n);
+void ft_bzero(void *s, size_t n);
 size_t ft_strlcpy(char *dst, const char *src, size_t size);
 size_t ft_strlcat(char *dst, const char *src, size_t size);
+void *ft_memchr(const void *s, int c, size_t n);
 
 #endif
