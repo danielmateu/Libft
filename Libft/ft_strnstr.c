6a47048e2eccb4f1ae70e8aad48336bf7b764ef1
daniel_mateu <danielmateu86@gmail.com>
2024-01-15 13:47:44 +0100

    ft_strnstr updated

--- Libft/ft_strnstr.c
+++ Libft/ft_strnstr.c
@@ -1,20 +1,20 @@
 #include "libft.h"
 
-char *ft_strnstr(const char *big, const char *little, size_t len)
+char *ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
     size_t i;
     size_t j;
 
     i = 0;
-    if (little[0] == '\0')
-        return ((char *)big);
-    while (big[i] != '\0' && i < len)
+    if (needle[0] == '\0')
+        return ((char *)haystack);
+    while (haystack[i] != '\0' && i < len)
     {
         j = 0;
-        while (big[i + j] == little[j] && i + j < len)
+        while (haystack[i + j] == needle[j] && i + j < len)
         {
-            if (little[j + 1] == '\0')
-                return ((char *)big + i);
+            if (needle[j + 1] == '\0')
+                return ((char *)haystack + i);
             j++;
         }
         i++;
