ba0e7e34631a3340e7ee37ed42aed559e40c44a7
daniel_mateu <danielmateu86@gmail.com>
2024-01-17 12:38:55 +0100

    ft_strdup.c added

---
+++ Libft/ft_strdup.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: damateu- <damateu-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/17 12:36:04 by damateu-          #+#    #+#             */
+/*   Updated: 2024/01/17 12:38:28 by damateu-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+/*
+    Definicion: Duplica una cadena de caracteres.
+    Parametros: 
+        - s1: Cadena de caracteres a duplicar.
+    Retorno:
+        - Cadena de caracteres duplicada.
+*/
+
+int    ft_strlen(const char *str)
+{
+    int i;
+
+    i = 0;
+    while (str[i])
+        i++;
+    return (i);
+}
+
+char    *ft_strdup(const char *s1)
+{
+    char    *s2;
+    int     i;
+
+    i = 0;
+    s2 = (char *)malloc(sizeof(char) * ft_strlen(s1) + 1);
+    if (!s2)
+        return (NULL);
+    while (s1[i])
+    {
+        s2[i] = s1[i];
+        i++;
+    }
+    s2[i] = '\0';
+    return (s2);
+}
+
+int main()
+{
+    char *s1 = "Hola";
+    char *s2 = ft_strdup(s1);
+    printf("%s\n", s2);
+    return (0);
+}
--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: damateu- <damateu-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 11:41:23 by damateu-          #+#    #+#             */
-/*   Updated: 2024/01/17 12:04:11 by damateu-         ###   ########.fr       */
+/*   Updated: 2024/01/17 12:35:42 by damateu-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,30 +19,31 @@
 #include <unistd.h>
 #include <stddef.h>
 
-int ft_isalpha(int c);
-int ft_isalnum(int c);
-int ft_isdigit(int c);
-int ft_strlen(const char *str);
-int ft_isascii(int c);
-int ft_isprint(int c);
-int ft_toupper(int c);
-int ft_tolower(int c);
-char *ft_strchr(const char *s, int c);
-char *ft_strrchr(const char *s, int c);
-void *ft_memset(void *b, int c, size_t len);
-void *ft_memmove(void *dst, void const *src, size_t n);
-int ft_strncmp(const char *str, const char *str2, size_t n);
-void ft_bzero(void *s, size_t n);
-size_t ft_strlcpy(char *dst, const char *src, size_t size);
-size_t ft_strlcat(char *dst, const char *src, size_t size);
-void *ft_memchr(const void *s, int c, size_t n);
-int ft_memcmp(const void *s1, const void *s2, size_t n);
-char *ft_strnstr(const char *big, const char *little, size_t len);
-int ft_atoi(const char *nptr);
+int     ft_isalpha(int c);
+int     ft_isalnum(int c);
+int     ft_isdigit(int c);
+int     ft_strlen(const char *str);
+int     ft_isascii(int c);
+int     ft_isprint(int c);
+int     ft_toupper(int c);
+int     ft_tolower(int c);
+char    *ft_strchr(const char *s, int c);
+char    *ft_strrchr(const char *s, int c);
+void    *ft_memset(void *b, int c, size_t len);
+void    *ft_memmove(void *dst, void const *src, size_t n);
+int     ft_strncmp(const char *str, const char *str2, size_t n);
+void    ft_bzero(void *s, size_t n);
+size_t  ft_strlcpy(char *dst, const char *src, size_t size);
+size_t  ft_strlcat(char *dst, const char *src, size_t size);
+void    *ft_memchr(const void *s, int c, size_t n);
+int     ft_memcmp(const void *s1, const void *s2, size_t n);
+char    *ft_strnstr(const char *big, const char *little, size_t len);
+int     ft_atoi(const char *nptr);
 
-void *ft_calloc(size_t count, size_t size);
+void    *ft_calloc(size_t count, size_t size);
+char    *ft_strdup(const char *s1);
 
-char *ft_substr(char const *s, unsigned int start, size_t len);
-char *ft_strjoin(char const *s1, char const *s2);
+char    *ft_substr(char const *s, unsigned int start, size_t len);
+char    *ft_strjoin(char const *s1, char const *s2);
 
 #endif
