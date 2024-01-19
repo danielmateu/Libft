7aff2bc55bf28f3e32838fffbc98b5d17bcf94af
Daniel Mateu <danielmateu86@gmail.com>
2024-01-19 14:09:47 +0100

    ft_itoa added

---
+++ Libft/ft_itoa.c
@@ -0,0 +1,15 @@
+#include "libft.h"
+/*
+** Allocates (with malloc(3)) and returns a string representing the integer received as an argument.
+** Negative numbers must be handled.
+** Return value: The string representing the integer. NULL if the allocation fails.
+
+** Parameters: #1. The integer to convert.
+** External functs: malloc
+*/
+
+char *ft_itoa(int n)
+{
+
+
+}
