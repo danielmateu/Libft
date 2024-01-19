5740db0bcfeb8b30f9999305e5ab64a4174aa263
Daniel Mateu <danielmateu86@gmail.com>
2024-01-19 14:45:30 +0100

    ft_itoa.c updated

--- Libft/ft_itoa.c
+++ Libft/ft_itoa.c
@@ -8,8 +8,30 @@
 ** External functs: malloc
 */
 
-char *ft_itoa(int n)
+char    *ft_itoa(int n)
 {
+    long    num;
+    int     len;
+    int     sign;
+    char    *str;
 
-
+    sign = (n < 0) ? -1 : 1;
+    num = (long)n * sign;
+    len = (n <= 0) ? 1 : 0;
+    while (n)
+    {
+        n /= 10;
+        len++;
+    }
+    if (!(str = (char *)malloc(sizeof(char) * (len + 1))))
+        return (NULL);
+    str[len] = '\0';
+    while (len--)
+    {
+        str[len] = num % 10 + '0';
+        num /= 10;
+    }
+    if (sign == -1)
+        str[0] = '-';
+    return (str);
 }
