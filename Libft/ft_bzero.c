fddef5d4abfa692f0be7103f6686923450240f9c
daniel_mateu <danielmateu86@gmail.com>
2024-01-23 14:30:44 +0100

    ft_bzero updated

--- Libft/ft_bzero.c
+++ Libft/ft_bzero.c
@@ -1,16 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: damateu- <damateu-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/23 14:29:20 by damateu-          #+#    #+#             */
+/*   Updated: 2024/01/23 14:30:07 by damateu-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+/*
+    Descripcion: Rellena con 0s un string
+    Parametros:
+        - s: string a rellenar
+        - n: numero de 0s a rellenar
+    Retorno: nada
+*/
 
 #include "libft.h"
 
-void    ft_bzero(void *s, size_t n)
+void	ft_bzero(void *s, size_t n)
 {
-    // memset(s, 0, n);
-    // sin usar memset
-    unsigned char *str;
+	unsigned char	*str;
 
-    str = (unsigned char *)s;
-    while (n--)
-        *str++ = 0;
+	str = (unsigned char *)s;
+	while (n--)
+		*str++ = 0;
 }
 /*
 int main()
