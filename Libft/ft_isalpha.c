a2c0beee3eedef6a1137259f21ea02a6c418779e
daniel_mateu <danielmateu86@gmail.com>
2024-01-23 14:34:23 +0100

    ft_isalpha updated

--- Libft/ft_isalpha.c
+++ Libft/ft_isalpha.c
@@ -6,9 +6,16 @@
 /*   By: damateu- <damateu-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 12:16:48 by damateu-          #+#    #+#             */
-/*   Updated: 2024/01/16 13:52:26 by damateu-         ###   ########.fr       */
+/*   Updated: 2024/01/23 14:34:07 by damateu-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*
+	Descripcion: Comprueba si el caracter es una letra
+	Parametros:
+		- c: caracter a comprobar
+	Retorno: 1 si es una letra, 0 si no lo es
+*/
 
 #include "libft.h"
 
