b35707f9f1d32de745e274ae82335240925e9b27
daniel_mateu <danielmateu86@gmail.com>
2024-01-24 14:00:35 +0100

    ft_lstsize_bonus.c added

---
+++ Libft/ft_lstsize_bonus.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstsize_bonus.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: damateu- <damateu-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/24 13:29:22 by damateu-          #+#    #+#             */
+/*   Updated: 2024/01/24 13:41:27 by damateu-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+    Descripcion:
+        Cuenta el numero de elementos de una lista
+    Parametros:
+        #1. lst -> El comienzo de la lista
+    Return:
+        El numero de elementos de la lista
+*/
+
+#include "libft.h"
+
+int ft_lstsize(t_list *lst)
+{
+    int i;
+
+    i = 0;
+    while (lst)
+    {
+        lst = lst->next;
+        i++;
+    }
+    return (i);
+}
+
+int main(void)
+{
+    t_list *lst;
+    t_list *lst2;
+    t_list *lst3;
+
+    lst = malloc(sizeof(t_list));
+    lst2 = malloc(sizeof(t_list));
+    lst3 = malloc(sizeof(t_list));
+    lst->content = "Hola";
+    lst2->content = "Mundo";
+    lst3->content = "Cruel";
+    lst->next = lst2;   
+    lst2->next = lst3;
+    lst3->next = NULL;
+    printf("%d\n", ft_lstsize(lst));
+    return (0);
+}
--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: damateu- <damateu-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/09 11:41:23 by damateu-          #+#    #+#             */
-/*   Updated: 2024/01/24 12:15:23 by damateu-         ###   ########.fr       */
+/*   Updated: 2024/01/24 13:31:32 by damateu-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,5 +64,6 @@ void	ft_putnbr_fd(int n, int fd);
 
 t_list *ft_lstnew(void *content);
 void    ft_lstadd_front(t_list **lst, t_list *new);
+int     ft_lstsize(t_list *lst);
 
 #endif
