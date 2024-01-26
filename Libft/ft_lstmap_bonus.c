b9aa6d0cc7f229a2a4049531ddbdc408700727b1
daniel_mateu <danielmateu86@gmail.com>
2024-01-26 13:00:19 +0100

    ft_lstmap_bonus.c updated

--- Libft/ft_lstmap_bonus.c
+++ Libft/ft_lstmap_bonus.c
@@ -18,28 +18,31 @@
 
 #include "libft.h"
 
-t_list  *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-    t_list *new;
-    t_list *tmp;
+	t_list	*new_lst;
+	t_list	*new_node;
+    void    *new_content;
 
-    if (!lst || !f)
-        return (NULL);
-    new = NULL;
-    while (lst)
-    {
-        tmp = ft_lstnew(f(lst->content));
-        if (!tmp)
-        {
-            ft_lstclear(&new, del);
-            return (NULL);
-        }
-        ft_lstadd_back(&new, tmp);
-        lst = lst->next;
-    }
-    return (new);
+	new_lst = NULL;
+	new_node = NULL;
+    new_content = NULL;
+	while (lst)
+	{
+        new_content = f(lst->content);
+		new_node = ft_lstnew(f(lst->content));
+		if (!new_node)
+		{
+            del(new_content);
+			ft_lstclear(&new_lst, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new_lst, new_node);
+		lst = lst->next;
+	}
+	return (new_lst);
 }
-
+/*
 int main(void)
 {
     // Test para ft_lstmap
@@ -64,3 +67,4 @@ int main(void)
     printf("%s\n", lst->content);
     return (0);
 }
+*/
